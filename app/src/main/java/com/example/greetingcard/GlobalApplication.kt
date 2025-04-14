c087773d0682bc2ed3c88b0227e2daa85e4d2826
SuuuHyeon <tngutnqls6909@gmail.com>
2025-04-14 19:50:39 +0900

    Merge pull request #9 from IronBirdProject/ir-27
    Ir 27

--- app/src/main/java/com/example/greetingcard/GlobalApplication.kt
+++ app/src/main/java/com/example/greetingcard/GlobalApplication.kt
@@ -2,11 +2,13 @@ package com.example.greetingcard
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import com.kakao.vectormap.KakaoMapSdk
 
 class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
         KakaoSdk.init(this, "aa0d4de155389c64d11681477ffe0ad6")
+        KakaoMapSdk.init(this, "LBVpzlH+WOk0lfW6q4/xB/kMp6w=")
     }
 }
